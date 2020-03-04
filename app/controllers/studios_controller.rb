a61061dc4b01f1f9af6927e31e201e42e5ad0b7a
github username <github email address>
2020-03-04 16:19:56 -0500

    added helper methods to application controller

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -15,7 +15,7 @@ class StudiosController < ApplicationController
 
     def create
         @studio = Studio.create(studio_params)
-        redirect_to @studio
+        redirect_to user_studio_path(@current_user, @studio)
     end
 
     def show
