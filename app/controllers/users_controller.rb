294177a01cee3ccec57f44d49bcdba7693a4ddad
github username <github email address>
2020-03-04 12:15:24 -0500

    user new, create, show actions/views made with form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+    before_action :set_user, :only => :show 
+
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user 
+            session[:user_id] = params[:id]
+            redirect_to @user
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    private
+
+    def user_params
+        params.require(@user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 end
