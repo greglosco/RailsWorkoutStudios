593bc28222954154b7e01fac0c2ee1d3a654284d
github username <github email address>
2020-03-04 14:00:29 -0500

    studio actions drawn

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,2 +1,18 @@
 class StudiosController < ApplicationController
+
+    def new
+        @studio = Studio.new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    private
+
+    def studio_params
+        params.require(:studio).require(:name, :category)
+    end
 end
