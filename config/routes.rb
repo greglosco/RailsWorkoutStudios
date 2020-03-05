905601a18f6832441508286da57e51330dba3cdb
github username <github email address>
2020-03-05 17:58:53 -0500

    added omniauth route and link on welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/login', :to => 'sessions#new'
   post '/login', :to => 'sessions#create'
   get '/logout', :to => 'sessions#destroy'  
+  get '/auth/github/callback' => 'sessions#create'
   
   resources :reviews
   resources :studios do
