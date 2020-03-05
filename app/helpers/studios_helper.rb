cded1198131538f0cc43a7284933045ff3383821
github username <github email address>
2020-03-04 22:28:06 -0500

    moved button_text helper method to studios helper module

--- app/helpers/studios_helper.rb
+++ app/helpers/studios_helper.rb
@@ -1,2 +1,13 @@
 module StudiosHelper
+
+    def button_text
+        if controller.action_name == "new"
+           return "Add"
+        elsif controller.action_name == "edit"
+           return "Update"
+        else
+           return "Submit"
+        end
+    end
+
 end
