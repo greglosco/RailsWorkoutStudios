df57b9513794be87ab8dd5c2abdab476640dca01
github username <github email address>
2020-03-05 00:21:24 -0500

    edited collection tag, still not working, going to bed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,7 +16,7 @@ class ReviewsController < ApplicationController
     private
 
     def review_params
-        params.require(:review).permit(:studio_name, :rating, :content, :user_id)
+        params.require(:review).permit(:studio_name, :rating, :content, :user_id, :studio_id)
     end
 
     def set_review
