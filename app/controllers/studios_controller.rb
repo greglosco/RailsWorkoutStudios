f5cc51f72aeb5d38c0a318d7fb00443bf9ebef3f
github username <github email address>
2020-03-04 21:57:03 -0500

    added name: & category: html to studio show page

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -4,7 +4,8 @@ class StudiosController < ApplicationController
     before_action :set_studio, :only => :show
 
     def index
-        @studios = User.find(params[:user_id]).studios
+        @user = User.find_by(:id => params[:user_id])
+        @studios = @user.studios
     end
 
     def new
