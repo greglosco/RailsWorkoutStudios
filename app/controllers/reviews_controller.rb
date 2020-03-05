c0c7af1bd2eb2d36923f380eb145efc7cf5862c1
github username <github email address>
2020-03-04 23:36:51 -0500

    stubbed out new, create, show actions & strong params + set methods for review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :content)
+    end
+
+    def set_review
+        @review = Review.find(params[:id])
+    end
+
+
 end
