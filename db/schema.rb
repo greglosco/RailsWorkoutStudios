fde82e60a38b31427da6493d533640ebdd1c31af
Greg Losco <gtlosco@gmail.com>
2020-05-22 18:24:16 -0700

    added puma as server in procfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_020232) do
+ActiveRecord::Schema.define(version: 2020_05_13_003136) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "fitness_activities", force: :cascade do |t|
+    t.string "exercise"
+    t.date "date"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hobbies", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "livestreams", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name"
+    t.string "teamname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.string "team"
+    t.string "position"
+    t.bigint "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_players_on_owner_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "content"
-    t.integer "user_id"
-    t.integer "studio_id"
+    t.bigint "user_id"
+    t.bigint "studio_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["studio_id"], name: "index_reviews_on_studio_id"
@@ -26,7 +78,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_020232) do
   create_table "studios", force: :cascade do |t|
     t.string "name"
     t.string "category"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_studios_on_user_id"
@@ -41,4 +93,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_020232) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "players", "owners"
+  add_foreign_key "reviews", "studios"
+  add_foreign_key "reviews", "users"
 end
